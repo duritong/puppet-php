0349d1c13fbf450428b7957cc4fa6cca4d796706
am <am@d66ca3ae-40d7-4aa7-90d4-87d79ca94279>
2008-02-29 16:09:39 +0000

    typo
    git-svn-id: https://svn/ipuppet/trunk/modules/php@941 d66ca3ae-40d7-4aa7-90d4-87d79ca94279

--- manifests/init.pp
+++ manifests/init.pp
@@ -61,18 +61,19 @@ define php::gentoo {
     }
    
     # config files
-    file{"/etc/php/apache2-php5/php.ini":
-        source => [
-            "puppet://$server/dist/php/apache2_php5_php.ini/${fqdn}/php.ini",
-            "puppet://$server/php/apache2_php5_php.ini/${fqdn}/php.ini",
-            "puppet://$server/php/apache2_php5_php.ini/default/php.ini",
-        ],
-        owner => root,
-        group => 0,
-        mode => 0644,
-        require => Package[php],
-        require => Package[apache],
-        notify => Service[apache],
+    file{
+        "/etc/php/apache2-php5/php.ini":
+	        source => [
+	            "puppet://$server/dist/php/apache2_php5_php.ini/${fqdn}/php.ini",
+	            "puppet://$server/php/apache2_php5_php.ini/${fqdn}/php.ini",
+	            "puppet://$server/php/apache2_php5_php.ini/default/php.ini"
+	        ],
+	        owner => root,
+	        group => 0,
+	        mode => 0644,
+	        require => Package[php],
+	        require => Package[apache],
+	        notify => Service[apache],
     }
 }
 
