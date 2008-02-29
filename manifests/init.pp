f2046fc5ec64ae3dd362998ed9f30e944d1cda2c
andreas <andreas@immerda.ch>
2008-02-29 15:35:11 +0000

    php aktivieren für sicher.immerda

---
+++ manifests/init.pp
@@ -0,0 +1,63 @@
+# php/manifests/init.pp - various ways of installing php
+# Copyright (C) 2007 David Schmitt <david@schmitt.edv-bus.at>
+# See LICENSE for the full license granted to you.
+
+class php {
+    case $operatingsystem {
+        debian: { include php::debian }
+        centos: { include php::centos }
+        ubuntu: { include php::ubuntu }
+        gentoo: { include php::gentoo }
+    }
+}
+
+class php::centos {}
+class php::ubuntu {}
+
+define php::debian::pear ($version = '') {
+	include "php::debian::pear::common"
+
+	package { "php${version}-${name}": ensure => installed }
+}
+
+class php::debian::pear::common {
+	package { ["php-pear", "php5-common" ]: ensure => installed }
+}
+
+class php::debian {
+
+	package { [ "php5", "php5-cli", "libapache2-mod-php5", "phpunit2" ]: ensure => installed }
+
+	php::debian::pear { [
+		"auth-pam", "curl", "idn", "imap", "json", "ldap", "mcrypt", "mhash",
+		"ming", "mysql", "odbc", "pgsql", "ps", "pspell", "recode", "snmp",
+		"sqlite", "sqlrelay", "tidy", "uuid", "xapian", "xmlrpc", "xsl"
+		]:
+			version => 5
+	}
+
+	include "php::debian::common"
+}
+
+
+
+class php::debian::common {
+	php::pear {
+		[ auth, benchmark, cache, cache-lite, date, db, file, fpdf, gettext,
+		html-template-it, http, http-request, log, mail, mail-mime, net-checkip,
+		net-dime, net-ftp, net-imap, net-ldap, net-sieve, net-smartirc, net-smtp,
+		net-socket, net-url, pager, radius, simpletest, services-weather, soap,
+		sqlite3, xajax, xml-parser, xml-serializer, xml-util ]:
+	}
+}
+
+define php::gentoo {
+   package { 'php':
+        ensure => present,
+        category => $operatingsystem ? {
+            gentoo => 'dev-lang',
+            default => '',
+        }
+    }
+}
+
