5525b0d9a25038386a02a7a9e4b445e8ed242369
mh <mh@immerda.ch>
2008-09-05 11:33:34 +0000

    removed dependency

--- manifests/packages/geoip.pp
+++ manifests/packages/geoip.pp
@@ -1,9 +1,10 @@
 # manifests/packages/geoip.pp
 
 class php::packages::geoip {
+
     include geoip
+
     php::pecl{'geoip': 
         mode => 'cli', 
-        require => Package['GeoIP'],
     }
 }
