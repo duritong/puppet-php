f7841f167d5a5a6a9c08445d4ab6ef01da5211fb
mh <mh@immerda.ch>
2008-09-05 12:42:44 +0000

    removed double definition

--- manifests/init.pp
+++ manifests/init.pp
@@ -56,15 +56,14 @@ class php::centos inherits php::base {
 class php::centos::common {
     package{ 
         [ 'php-common', 'php-idn', 'php-tidy', 
-            'php-gd', 'php-pear-XML-Serializer', 
-            'php-pear-Cache-Lite', 'php-mhash' ]:
+            'php-gd', 'php-mhash' ]:
         ensure => installed,
         require => Package['php'],
     }
 
     php::pecl{'Fileinfo': }
-    php::pear{ [ 'MDB2', 'MDB2-Driver-pgsql', 'MDB2-Driver-mysql',
-                'Cache-Lite', 'Date-Holidays' ]: 
+    php::pear{ [ 'MDB2', 'MDB2-Driver-pgsql', 'MDB2-Driver-mysql', 
+                'Cache-Lite', 'Date-Holidays', 'XML-Serializer' ]: 
     }
 }
 
