8c8c3c4324fb9c772fefa8a3d233d261b1eeead2
mh <mh@d66ca3ae-40d7-4aa7-90d4-87d79ca94279>
2008-09-05 12:35:42 +0000

    fixed location
    git-svn-id: https://svn/ipuppet/trunk/modules/php@2197 d66ca3ae-40d7-4aa7-90d4-87d79ca94279

--- manifests/init.pp
+++ manifests/init.pp
@@ -275,7 +275,7 @@ class php::pear::common::cli {
     # updates for pear installations
     # we put a Z in front to ensure that it gets executed after the dail yum update
     file{'/etc/cron.daily/Z_pear_upgrade.sh':
-        source => "puppet://$server/php/pecl/$operatingsystem/pear_upgrade.sh",
+        source => "puppet://$server/php/pear/$operatingsystem/pear_upgrade.sh",
         owner => root, group => 0, mode => 0700;
     }
 }
