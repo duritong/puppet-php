db1dbc5ba5385095c2c9fe6128955d069df86494
mh <mh@d66ca3ae-40d7-4aa7-90d4-87d79ca94279>
2008-09-08 22:25:00 +0000

    we now have for that one as well a package
    git-svn-id: https://svn/ipuppet/trunk/modules/php@2225 d66ca3ae-40d7-4aa7-90d4-87d79ca94279

--- manifests/packages/idn.pp
+++ manifests/packages/idn.pp
@@ -1,11 +1,5 @@
 # manifests/packages/idn.pp
 
 class php::packages::idn {
-    include libidn::devel
-
-    php::pecl{'idn':
-        mode => 'cli',
-        state => 'beta',
-        target_mode => 'pear',
-    }
+    php::pecl{'idn': }
 }
