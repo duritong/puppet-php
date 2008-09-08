b6805fba23b6de0181ee64677d128f65e962109e
mh <mh@immerda.ch>
2008-09-08 21:51:43 +0000

    ssh2 is now installed by an rpm

--- manifests/packages/ssh2.pp
+++ manifests/packages/ssh2.pp
@@ -1,9 +1,5 @@
 # manifests/packages/ssh2.pp
 
 class php::packages::ssh2 {
-    include sshd::libssh2::devel
-
-    php::pecl{'ssh2-beta':
-        mode => 'cli',
-    }
+    php::pecl{'ssh2-beta': }
 }
