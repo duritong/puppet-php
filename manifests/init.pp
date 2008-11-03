cb0d52188b8e16acbadb1181aa3ca3d7a0edcd6e
Marcel Haerry <haerry@puzzle.ch>
2008-11-03 16:58:42 +0100

    added config file for httpd to manage

--- manifests/init.pp
+++ manifests/init.pp
@@ -31,12 +31,12 @@ class php::base {
 	        "puppet://$server/files/php/php.ini",
 	        "puppet://$server/php/config/php.ini"
 	    ],
-	    owner => root,
-	    group => 0,
-	    mode => 0644,
 	    require => [ Package[php], Package[apache] ],
 	    notify => Service[apache],
+	    owner => root, group => 0, mode => 0644;
     }
+
+    
 
     include php::suhosin
     include php::apc
@@ -49,6 +49,17 @@ class php::centos inherits php::base {
         Package[php]{
             require => Yum::Managed_yumrepo['remi'],
         }
+    }
+
+    file{'/etc/httpd/conf.d/php.conf':
+        source => [
+            "puppet://$server/files/php/apache/${operatingsystem}/${fqdn}/php.conf",
+            "puppet://$server/files/php/apache/${operatingsystem}/php.conf",
+            "puppet://$server/php/apache/${operatingsystem}/php.conf"
+        ],
+	    require => [ Package[php], Package[apache] ],
+	    notify => Service[apache],
+	    owner => root, group => 0, mode => 0644;
     }
 }
 
