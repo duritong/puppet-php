aab0ac7dd14872ffeb6b70dd324acf00b0f3ec24
mh <mh@immerda.ch>
2009-11-21 19:03:21 +0100

    json is also included in the testing repo

--- manifests/extensions/common.pp
+++ manifests/extensions/common.pp
@@ -6,13 +6,12 @@ class php::extensions::common {
             'gd', 'mhash' ]:
         mode => 'direct',
     }
-    case $php_centos_use_remi {
-        true: { info("php-pecl-json is included in php-common of remi") }
-        default: {
-            php::package{'json':
-                mode => 'pecl',
-            }
-        }
+    if $php_centos_use_remi or $php_centos_use_testing {
+      info("php-pecl-json is included in php-common of remi")
+    } else {
+      php::package{'json':
+        mode => 'pecl',
+      }
     }
     include php::pear::common
 }
