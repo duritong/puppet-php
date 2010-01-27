cde77ab684d3a3e53231d6e8f34250cc71c78ff0
mh <mh@immerda.ch>
2010-01-27 02:02:57 +0100

    fix testing inclusion

--- manifests/suhosin.pp
+++ manifests/suhosin.pp
@@ -2,7 +2,7 @@
 class php::suhosin {
     case $operatingsystem {
         centos: {
-            if $php_centos_use_remi {
+            if $php_centos_use_remi or $php_centos_use_testing {
                 include php::suhosin::package
             }
         }
