b6f448af8a1d3786e1f91c330e4daa1f423bb048
mh <mh@immerda.ch>
2010-08-11 16:56:02 +0200

    introduce itk_plus mode for mod_php

---
+++ manifests/itk_plus.pp
@@ -0,0 +1,5 @@
+class php::itk_plus inherits php::itk {
+    case $operatingsystem {
+        centos: { include php::itk_plus::centos }
+    }
+}
---
+++ manifests/itk_plus/centos.pp
@@ -0,0 +1,10 @@
+
+class php::itk_plus::centos inherits php::itk::centos {
+    File['/etc/httpd/conf.d/php.conf']{
+        source => [
+            "puppet:///modules/site-php/apache_itk_plus/${operatingsystem}/${fqdn}/php.conf",
+            "puppet:///modules/site-php/apache_itk_plus/${operatingsystem}/php.conf",
+            "puppet:///modules/php/apache_itk_plus/${operatingsystem}/php.conf"
+        ],
+    }
+}
