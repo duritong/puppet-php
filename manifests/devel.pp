ed8467dae4aa311049eed49fa01076331bf4ff93
mh <mh@immerda.ch>
2011-11-20 16:08:30 +0100

    we need php-devel for these upgrades

--- manifests/devel.pp
+++ manifests/devel.pp
@@ -1,8 +1,6 @@
-# manifests/devel.pp
-
 class php::devel {
-    package{"php-devel.${architecture}":
-        ensure => installed,
-        require => Package['php'],
-    }
+  package{"php-devel.${architecture}":
+    ensure => installed,
+    require => Package['php'],
+  }
 }
--- manifests/pear/common/cli.pp
+++ manifests/pear/common/cli.pp
@@ -1,11 +1,10 @@
-# manifests/pear/common/cli.pp
-
 class php::pear::common::cli {
-    # updates for pear installations
-    # we put a Z in front to ensure that it gets executed after the dail yum update
-    file{'/etc/cron.daily/Z_pear_upgrade.sh':
-        source => "puppet:///modules/php/pear/$operatingsystem/pear_upgrade.sh",
-        owner => root, group => 0, mode => 0700;
-    }
+  require php::devel
+  # updates for pear installations
+  # we put a Z in front to ensure that it gets executed after the dail yum update
+  file{'/etc/cron.daily/Z_pear_upgrade.sh':
+    source => "puppet:///modules/php/pear/$operatingsystem/pear_upgrade.sh",
+    owner => root, group => 0, mode => 0700;
+  }
 }
 
