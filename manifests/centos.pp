946c791bb156e8cd35647aa3f6071d3a188bee7a
mh <mh@immerda.ch>
2012-12-07 17:50:58 +0100

    these repos are only needed on EL5

--- manifests/centos.pp
+++ manifests/centos.pp
@@ -1,24 +1,30 @@
+# centos specific php stuff
 class php::centos inherits php::base {
+
+  if $::operatingsystemrelease < 6 {
     if $php::centos_use_remo {
-        include yum::remi
-        Package[php]{
-            require => Yum::Managed_yumrepo['remi'],
-        }
+      include yum::remi
+      Package[php]{
+        require => Yum::Managed_yumrepo['remi'],
+      }
     }
     if $php::centos_use_testing {
-        include yum::centos::testing
-        Package[php]{
-            require => Yum::Managed_yumrepo['centos5-testing'],
-        }
+      include yum::centos::testing
+      Package[php]{
+        require => Yum::Managed_yumrepo['centos5-testing'],
+      }
     }
-    file{'/etc/httpd/conf.d/php.conf':
-        source => [
-            "puppet:///modules/site_php/apache/${::operatingsystem}/${::fqdn}/php.conf",
-            "puppet:///modules/site_php/apache/${::operatingsystem}/php.conf",
-            "puppet:///modules/php/apache/${::operatingsystem}/php.conf"
-        ],
-      require => [ Package[php], Package[apache] ],
-      notify => Service[apache],
-      owner => root, group => 0, mode => 0644;
-    }
+  }
+  file{'/etc/httpd/conf.d/php.conf':
+    source  => [
+      "puppet:///modules/site_php/apache/${::operatingsystem}/${::fqdn}/php.conf",
+      "puppet:///modules/site_php/apache/${::operatingsystem}/php.conf",
+      "puppet:///modules/php/apache/${::operatingsystem}/php.conf"
+    ],
+    require => [ Package[php], Package[apache] ],
+    notify  => Service[apache],
+    owner   => root,
+    group   => 0,
+    mode    => '0644';
+  }
 }
