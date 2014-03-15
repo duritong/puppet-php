2105e98873dff386363c46fcbc6099ebfd6cc0ff
o <o@immerda.ch>
2014-03-15 18:32:39 +0100

    install correct pckg

--- manifests/packages/pecl_http.pp
+++ manifests/packages/pecl_http.pp
@@ -1,5 +1,11 @@
 class php::packages::pecl_http {
-  php::package{'http':
-    mode => 'pecl',
+  if ($::operatingsystem == 'centos' and $::lsbmajdistrelease == '6') {
+    php::package{'http1':
+      mode => 'pecl',
+    }
+  } else {
+    php::package{'http':
+      mode => 'pecl',
+    }
   }
 }
