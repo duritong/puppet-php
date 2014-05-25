ded21e7a9055414cde422e3a4b57474b331f05df
mh <mh@immerda.ch>
2014-05-25 17:06:51 +0200

    linting

--- manifests/base.pp
+++ manifests/base.pp
@@ -1,20 +1,23 @@
+# setup php
 class php::base {
-  package{php:
+  package{'php':
     ensure => present,
     notify => Service[apache],
   }
-  file{php_ini_config:
-    path => '/etc/php.ini',
-    source => [
+  file{'php_ini_config':
+    path    => '/etc/php.ini',
+    source  => [
       "puppet:///modules/site_php/${::fqdn}/php.ini",
       "puppet:///modules/site_php/${php::cluster_node}/php.ini",
-      "puppet:///modules/site_php/php.ini",
+      'puppet:///modules/site_php/php.ini',
       "puppet:///modules/php/config/php.ini.${::architecture}",
-      "puppet:///modules/php/config/php.ini"
+      'puppet:///modules/php/config/php.ini'
     ],
     require => [ Package[php], Package[apache] ],
-    notify => Service[apache],
-    owner => root, group => 0, mode => 0644;
+    notify  => Service[apache],
+    owner   => root,
+    group   => 0,
+    mode    => '0644';
   }
 
   include php::suhosin
