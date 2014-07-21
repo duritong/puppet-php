e776fe29668d489e45c3f341f4af74ceda167668
o <o@immerda.ch>
2014-07-20 22:53:55 -0400

    that was wrong

--- manifests/packages/pecl_http.pp
+++ manifests/packages/pecl_http.pp
@@ -1,11 +1,5 @@
 class php::packages::pecl_http {
-  if ($::operatingsystem == 'centos' and $::lsbmajdistrelease == '6') {
-    php::package{'http1':
-      mode => 'pecl',
-    }
-  } else {
-    php::package{'http':
-      mode => 'pecl',
-    }
+  php::package{'http':
+    mode => 'pecl',
   }
 }
