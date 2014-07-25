f2ba8889d61b693a4b03bbf9a32267942580c700
mh <mh@immerda.ch>
2014-07-25 16:24:06 +0200

    don't do stupid things like that

--- files/pear/CentOS/pear_upgrade.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-pear update-channels > /dev/null && pear upgrade > /dev/null
-pecl update-channels > /dev/null && pecl upgrade > /dev/null
--- manifests/pear/common/cli.pp
+++ manifests/pear/common/cli.pp
@@ -2,11 +2,5 @@ class php::pear::common::cli {
   require php::pecl::common
   require php::pear::common
   require php::devel
-  # updates for pear installations
-  # we put a Z in front to ensure that it gets executed after the dail yum update
-  file{'/etc/cron.daily/Z_pear_upgrade.sh':
-    source => "puppet:///modules/php/pear/${::operatingsystem}/pear_upgrade.sh",
-    owner => root, group => 0, mode => 0700;
-  }
 }
 
