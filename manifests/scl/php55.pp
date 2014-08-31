14d48e698d8f747b82abfe014ed921961e89cefd
mh <mh@immerda.ch>
2014-08-31 13:50:08 +0200

    fix uri and name

--- manifests/scl/php55.pp
+++ manifests/scl/php55.pp
@@ -1,12 +1,12 @@
 # manage an scl php54 installation
-class php::scl::php54 {
-  require ::scl::php54
-  file{'/opt/rh/php54/root/etc/php.ini':
+class php::scl::php55 {
+  require ::scl::php55
+  file{'/opt/rh/php55/root/etc/php.ini':
     source  => [
-      "puppet:///modules/site_php/scl_php54/${::fqdn}/php.ini",
-      "puppet:///modules/site_php/scl_php54/${php::cluster_node}/php.ini",
-      'puppet:///modules/site_php/scl_php54/php.ini',
-      'puppet:///modules/php/config/scl_php54/php.ini'
+      "puppet:///modules/site_php/scl_php55/${::fqdn}/php.ini",
+      "puppet:///modules/site_php/scl_php55/${php::cluster_node}/php.ini",
+      'puppet:///modules/site_php/scl_php55/php.ini',
+      'puppet:///modules/php/config/scl_php55/php.ini'
     ],
     notify  => Service[apache],
     owner   => root,
