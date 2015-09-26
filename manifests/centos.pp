48bd542695f55567ad6192adbf46911374b32ac0
mh <mh@immerda.ch>
2015-09-26 12:00:12 +0200

    drop EL5 support

--- manifests/centos.pp
+++ manifests/centos.pp
@@ -1,25 +1,10 @@
 # centos specific php stuff
 class php::centos inherits php::base {
-
-  if $::operatingsystemrelease < 6 {
-    if $php::centos_use_remo {
-      include yum::remi
-      Package[php]{
-        require => Yum::Managed_yumrepo['remi'],
-      }
-    }
-    if $php::centos_use_testing {
-      include yum::centos::testing
-      Package[php]{
-        require => Yum::Managed_yumrepo['centos5-testing'],
-      }
-    }
-  }
   file{'/etc/httpd/conf.d/php.conf':
     source  => [
       "puppet:///modules/site_php/apache/${::operatingsystem}/${::fqdn}/php.conf",
       "puppet:///modules/site_php/apache/${::operatingsystem}/php.conf",
-      "puppet:///modules/php/apache/${::operatingsystem}/php.conf"
+      "puppet:///modules/php/apache/${::operatingsystem}/php.conf",
     ],
     require => [ Package[php], Package[apache] ],
     notify  => Service[apache],
