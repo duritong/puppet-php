e5312fe4ec3d034ed980093b8cbe8bc1e25d4fef
mh <mh@immerda.ch>
2018-11-04 21:50:06 +0100

    some output to log

--- files/fpm/kill-pool.sh
+++ files/fpm/kill-pool.sh
@@ -5,6 +5,7 @@ for fpm in /run/fpm-*/pid; do
   CHILDREN=$(ps -o pid --no-headers --ppid `cat ${fpm}` | wc -l)
 
   if [ "${CHILDREN}" == "0" ]; then
+    echo "Killing $(basename $(dirname ${fpm}))"
     kill `cat ${fpm}`
   fi
 done
