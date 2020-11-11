00f8d251bcc2f1287188aa7dd225d62283aaf5b2
mh <mh@immerda.ch>
2020-11-11 22:28:02 +0100

    add new class

---
+++ manifests/fpm/munin.pp
@@ -0,0 +1,11 @@
+# munin plugin to monitor fpm processes
+class php::fpm::munin {
+  munin::plugin::deploy{'php-fpm-':
+    ensure => absent,
+    source => 'fpm/php-fpm-munin.php'
+  } -> munin::plugin{
+    ['php-fpm-memory', 'php-fpm-cpu',
+      'php-fpm-count', 'php-fpm-time']:
+    ensure  => 'php-fpm-',
+  }
+}
