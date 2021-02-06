9d110acefcc3cb8b1ac9184da4302e41f5463d52
mh <mh@immerda.ch>
2021-02-06 23:34:56 +0100

    modernize

--- manifests/extensions/common.pp
+++ manifests/extensions/common.pp
@@ -1,19 +1,8 @@
 # common php extensions
 class php::extensions::common {
   php::package {
-    ['common', 'tidy', 'gd']:
+    ['common', 'tidy', 'gd', 'intl']:
       mode => 'direct';
-  }
-  if ($facts['os']['name'] == 'CentOS') and ($facts['os']['release']['major'] == '5') {
-    php::package {
-      'mhash':
-        mode => 'direct';
-    }
-  } else {
-    php::package {
-      'intl':
-        mode => 'direct';
-    }
   }
   include php::pear::common
 }
