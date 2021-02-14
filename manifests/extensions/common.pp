8502e0a64604bf9bb565aac789c26e3a0c973719
mh <mh@immerda.ch>
2021-02-14 14:55:33 +0100

    not anymore available on EL8 as well

--- manifests/extensions/common.pp
+++ manifests/extensions/common.pp
@@ -1,8 +1,13 @@
 # common php extensions
 class php::extensions::common {
   php::package {
-    ['common', 'tidy', 'gd', 'intl']:
+    ['common', 'gd', 'intl']:
       mode => 'direct';
+  }
+  if versioncmp($facts['os']['release']['major'],'8') < 0 {
+    php::package { 'tidy':
+      mode => 'direct';
+    }
   }
   include php::pear::common
 }
--- manifests/extensions/mysql.pp
+++ manifests/extensions/mysql.pp
@@ -1,6 +1,12 @@
 # manage mysql extension
 class php::extensions::mysql {
-  php::package { 'mysql':
-    mode => 'direct',
+  if versioncmp($facts['os']['release']['major'],'8') < 0 {
+    php::package { 'mysql':
+      mode => 'direct',
+    }
+  } else {
+    php::package { 'mysqlnd':
+      mode => 'direct',
+    }
   }
 }
