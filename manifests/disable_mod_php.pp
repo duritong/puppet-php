67572c8b613b333dd855a1a6505225e0f79d30ea
mh <mh@immerda.ch>
2022-02-09 22:40:45 +0100

    package php might not get installed at all

--- manifests/disable_mod_php.pp
+++ manifests/disable_mod_php.pp
@@ -4,9 +4,8 @@ class php::disable_mod_php {
   # which is obsolete here, but we want
   # to keep the file so an update of the
   # php rpm would not place it there again.
-  file { '/etc/httpd/conf.d/php.conf':
+  Package<| $title == 'php' |> -> file { '/etc/httpd/conf.d/php.conf':
     content => "# empty by intend as we are using mod_fcgid\n",
-    require => Package['php'],
     notify  => Service['apache'],
   }
 }
